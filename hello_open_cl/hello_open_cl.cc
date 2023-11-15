81aad50f5d78ce275ecc6f4b39e904a9c665a02b
Hajime Morrita <omo@dodgson.org>
2023-11-14 20:47:09 -0800

    dlopen libOpenCL.so

--- hello_open_cl/hello_open_cl.cc
+++ hello_open_cl/hello_open_cl.cc
@@ -1,7 +1,19 @@
 
 #include <iostream>
+#include <dlfcn.h>
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello?" << std::endl;
+
+    // TODO(morrita): Handle special casing. See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/delegates/gpu/cl/opencl_wrapper.cc
+    void* libopencl = dlopen("libOpenCL-pixel.so", RTLD_NOW | RTLD_LOCAL);
+    if (!libopencl) {
+    std::cout << "Failed to open libopencl!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Loaded!" << std::endl;
+    dlclose(libopencl);
+
     return 0;
 }
